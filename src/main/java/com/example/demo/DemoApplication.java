e36cf6e2def71da9ac92cb13b204c3e92451bfbd
lishuai <lishuai9584@163.com>
2022-11-30 16:34:42 +0800

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,10 @@
 package com.example.demo;
 
-        import lombok.val;
-        import org.camunda.bpm.engine.RuntimeService;
-        import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-        import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @Autowired
-    private RuntimeService runtimeService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
